6d7bc145b2a2f011dab7e1c2c3238ba2835b61df
Riley Childs <Riley@tfsgeo.com>
2014-03-30 10:27:32 -0400

    Added social media

--- index.php
+++ index.php
@@ -19,10 +19,12 @@ $first = $info['first'];
 $last = $info['last'];
 $title = $info['title'];
 $mail = $info['mail'];
+$facebook = $_SESSION['info']['faceebook'];
+$twitter = $_SESSION['info']['twitter'];
+$youtube = $_SESSION['info']['youtube'];
+$instagram = $_SESSION['info']['instagram'];
 
-echo <<<END
-
-
+echo <<<END 
 <div style="width:630px;float:left;">
 	<div style="float:left;width:155px;margin-right:10px;">
 		<img alt="Your logo" src="http://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/CUCA_Logo.jpg/120px-CUCA_Logo.jpg" style="margin-bottom:10px;" /> <a href="http://facebook.com/charlotteunitedchristian" style="margin-right:30px; float:left;"> <img alt="Facebook" src="https://sites.google.com/a/flashpanel.com/signatures/images/facebook-ico.png" /></a> <a href="http://twitter.com/cucawarriors" style="margin-right:30px; float:left;"> <img alt="Twitter" src="https://sites.google.com/a/flashpanel.com/signatures/images/twitter-ico.png" /></a></div>
@@ -30,6 +32,20 @@ echo <<<END
 		<span style="font-family:verdana,geneva,sans-serif;"><strong><span style="font-size: 20px; margin-bottom: 10px;">Charlotte United Chrisitan Academy</span></strong><br />
 		$first<span style="font-size: 12px;"> </span>$last<br />
 		<span style="font-size:12px;"><em>$title</em><br />
+		<span><em><center>
+END;
+	
+if (isset($facebook) and !$facebook == '') 
+		echo "<a href='http://facebook.com/$facebook' style='margin-right:20px;'''> <img alt='Facebook' style='height:20px; width:20px;' src='https://sites.google.com/a/flashpanel.com/signatures/images/facebook-ico.png' /></a>";
+if(isset($twitter) and !$twitter=='')
+		echo  "<a href='http://twitter.com/$twitter' style='margin-right:20px; '> <img style='height:20px; width:20px;' alt='Twitter' src='https://sites.google.com/a/flashpanel.com/signatures/images/twitter-ico.png' /></a>";
+if(isset($youtube) and !$youtube=='')
+	"<a href='http://youtube.com/$youtube' style='margin-right:20px; '> <img style='height=20px; width:20px;' alt='YouTube' src='http://library.duke.edu/imgs/blue-note/youtube30x30.png' /></a>";
+if(isset($instagram) and !$instagram == '')
+	echo "<a href='http://instagram.com/$instagram' style='margin-right:20px; '> <img style='height=20px; width:20px;' alt='YouTube' src='http://icons.iconarchive.com/icons/designbolts/free-instagram/256/Active-Instagram-3-icon.png'/></a>";
+
+echo <<<END
+</em></span></center>
 		<strong>P:</strong> 704-537-0031&nbsp;<span style="margin-left: 25px;"><strong>E: </strong><a href="mailto:$mail" style="text-decoration: none; color: inherit;">$mail</a></span><br />
 		7640 Wallace Rd, Charlotte, NC 28212</span></span><br />
 		<br />
@@ -123,10 +139,43 @@ else {
 }
 
 break;
-
 case 4:
+
+
 if (isset($_POST['mail'])) {
 $info['mail'] = $_POST['mail'];
+$_SESSION['info'] = $info;
+?>
+<i>Your Social Media, Put YOUR social media info here</i><br>
+<b>Please put your USERNAME only</b>
+<form method="post">
+<table border="1">
+	<tr><td>Facebook</td><td><input type='text' name='facebook'/></td></tr>
+	<tr><td>Twitter</td><td><input type='text' name='twitter'/></td></tr>
+	<tr><td>YouTube</td><td><input type='text' name='youtube'/></td></tr>
+	<tr><td>Instagram</td><td><input type='text' name='instagram'/></td></tr>
+</table></br>
+</form>
+}
+else {
+
+	echo "ERROR: <a href='?init=true'> Please go back and try again";
+
+}
+
+
+
+<?php
+break;
+
+case 5:
+if (isset($_POST['facebook'])) {
+$info['facebook'] = $_POST['facebook'];
+$info['twitter'] = $_POST['twitter'];
+$info['youtube'] = $_POST['youtube'];
+$info['instagram'] =$_POST['instagram'];
+
+
 $_SESSION['info'] = $info;
 ?>
 <i>Is this correct?</i><br>
@@ -135,6 +184,10 @@ $_SESSION['info'] = $info;
 	<tr><td>Last Name</td><td><?=$_SESSION['info']['last']?></td></tr>
 	<tr><td>Title</td><td><?=$_SESSION['info']['title']?></td></tr>
 	<tr><td>E-Mail</td><td><?=$_SESSION['info']['mail']?></td></tr>
+		<tr><td>Facebook</td><td><?=$_SESSION['info']['facebook']?></td></tr>
+	<tr><td>Twitter</td><td><?=$_SESSION['info']['twitter']?></td></tr>
+	<tr><td>YouTube</td><td><?=$_SESSION['info']['youtube']?></td></tr>
+	<tr><td>Instagram</td><td><?=$_SESSION['info']['instagram']?></td></tr>
 </table></br>
 <a href="?init=true" target="_parent"><button>No</button></a>&nbsp;<a href="?action=reveal"><button>Yes</button></a>
 
